5d3a288e102848b43dccfd54ae45a820a00e9dcd
Damian Nowak <foxnacity@gmail.com>
2025-10-13 19:24:52 +0200

    feat: init project with working dockercompose database and frontend

---
+++ backend/src/test/java/com/example/budgeter/BudgeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
